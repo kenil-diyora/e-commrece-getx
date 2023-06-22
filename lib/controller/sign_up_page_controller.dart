8f625b9ae9ae268f57688afd3780c28e031eaa84
kenil <kenil09.frontbitstd@gmail.com>
2023-06-22 15:17:49 +0530

    no change

--- lib/controller/sign_up_page_controller.dart
+++ lib/controller/sign_up_page_controller.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class SignUpPageController extends GetxController {
+class   SignUpPageController extends GetxController {
   final key = GlobalKey<FormState>();
   Rx<TextEditingController> email = TextEditingController().obs;
   Rx<TextEditingController> password = TextEditingController().obs;
